17c008bf6931c6fea748447e42b833029405b984
eftekher-alam <eftekher@yahoo.com>
2023-10-19 13:04:30 +0600

    environment file is setup and mongoDB connection is done

---
+++ coffee-store-client/src/components/coffee/UpdateCoffee.jsx
@@ -0,0 +1,10 @@
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            This is update coffee
+        </div>
+    );
+};
+
+export default UpdateCoffee;
--- coffee-store-server/index.js
+++ coffee-store-server/index.js
@@ -1,3 +1,5 @@
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -6,6 +8,34 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.MONGODB_USERNAME);
+
+
+
+const uri = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.ougk6tn.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        await client.connect();
+
+        console.log("Connection is stablish with database...");
+
+
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
 
 app.get("/", (req, res) => {
     res.send("Working.....!");
